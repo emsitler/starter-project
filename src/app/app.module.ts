5c09109d3819222819b3b0a92bc93907d4f30db2
emsitler <ems00030@mix.wvu.edu>
2023-02-02 13:20:13 -0500

    top navbar has all elements.
    To Do: formatting, spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './Navigation/top-nav-bar/top-nav-bar.component';
+import { BottomNavBarComponent } from './Navigation/bottom-nav-bar/bottom-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule
